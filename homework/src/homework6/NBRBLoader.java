0e13ad98535dffd8b981c29d3b8bcb4bf0f2bbba
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2020-06-28 18:21:02 +0300

    homework_6_1

---
+++ homework/src/homework6/NBRBLoader.java
@@ -0,0 +1,42 @@
+package homework6;
+
+/**
+ * Загрузчик курса с сайта Нац. Банка
+ */
+public class NBRBLoader extends SiteLoader {
+
+    /**
+     * Метод для запуска загрузки курса валют
+     *
+     * @param currencyName валюта которую мы ищем
+     * @return курс который мы нашли
+     */
+    @Override
+    public double load(SiteLoader.Currency currencyName) {
+        return load("https://www.nbrb.by/api/exrates/rates/" + currencyName.getId(), currencyName);
+    }
+
+    /**
+     * Обработка результата загрузки с сайта банка
+     *
+     * @param content      то что получилось загрузить
+     * @param currencyName валюта которую мы ищем
+     * @return курс который мы нашли
+     */
+    @Override
+    protected double handle(String content, SiteLoader.Currency currencyName) {
+        //TODO дописываем код сюда
+        double d1;
+        if (content.length() > 9) {
+            d1 = new Double(content.substring(content.length() - 8, content.length() - 2));
+            return d1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
+
+
+
+
---
+++ homework/src/homework6/Test.java
@@ -0,0 +1,14 @@
+package homework6;
+
+public class Test {
+    public static void main(String[] args) {
+        NBRBLoader loader = new NBRBLoader();
+
+        System.out.println(loader.load(SiteLoader.Currency.EUR));
+        System.out.println(loader.load(SiteLoader.Currency.RUS));
+        System.out.println(loader.load(SiteLoader.Currency.USD));
+
+
+
+    }
+}
